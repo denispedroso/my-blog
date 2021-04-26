e4398eb34060b6c6344ae923fe9404e811e1b606
Denis Pedroso <denisbpedroso@gmail.com>
2021-04-25 18:00:19 -0600

    First commit

---
+++ src/build/precache-manifest.9eb7eae76fc1ba5f2ee15c8b35cfddab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f86e358d885e79997be",
+    "url": "/static/css/main.2f872fcf.chunk.css"
+  },
+  {
+    "revision": "1f86e358d885e79997be",
+    "url": "/static/js/main.164707ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d72546995997ab1773b6",
+    "url": "/static/js/2.f96445f7.chunk.js"
+  },
+  {
+    "revision": "5186c90e8748cb35b759184df73bc87c",
+    "url": "/index.html"
+  }
+];
